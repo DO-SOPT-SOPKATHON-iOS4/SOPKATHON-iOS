5de5400843583517c61cdf564830fac65f1f53c1
leewoojye <whitehole36@naver.com>
2023-11-26 08:10:30 +0900

    feat [#26] 수정

---
+++ SOPKATHON-iOS/SOPKATHON-iOS/Network/Entity/CardEntity.swift
@@ -0,0 +1,20 @@
+//
+//  CardEntity.swift
+//  SOPKATHON-iOS
+//
+//  Created by Woo Jye Lee on 11/26/23.
+//
+
+struct CardEntity: Codable {
+    let nickname: String
+    let resultAge: Int
+    let title, content: String
+    let imgURL1: String
+    let imgURL2: String
+    
+    enum CodingKeys: String, CodingKey {
+        case nickname, resultAge, title, content
+        case imgURL1 = "imgUrl1"
+        case imgURL2 = "imgUrl2"
+    }
+}
