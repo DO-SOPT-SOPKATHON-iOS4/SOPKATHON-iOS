532f72787369c07a51a12fb524032feb956c365a
seuriseuljjeok <heeseul09@naver.com>
2023-11-26 01:59:01 +0900

    [Feat] navigation bar 틀 구현

--- SOPKATHON-iOS/SOPKATHON-iOS/Presentation/HistoryScene/View/HistoryView.swift
+++ SOPKATHON-iOS/SOPKATHON-iOS/Presentation/HistoryScene/View/HistoryView.swift
@@ -1,8 +1,83 @@
-//
-//  HistoryView.swift
-//  SOPKATHON-iOS
-//
-//  Created by 고아라 on 2023/11/26.
-//
+import UIKit
 
-import Foundation
+import SnapKit
+
+final class HistoryView: UIView {
+
+    // MARK: - Properties
+    
+//    var index: Int = 0
+    
+    // MARK: - UI Components
+    
+    private lazy var homeBtn: UIButton = {
+       let btn = UIButton()
+//        btn.setImage(<#T##image: UIImage?##UIImage?#>, for: .normal)
+        btn.backgroundColor = .systemGray2
+        return btn
+    }()
+        
+//    private let imageView: UIImageView = {
+//        let image = UIImageView()
+//        image.contentMode = .scaleAspectFill
+//        image.image = UIImage(systemName: "apple.logo")
+//        return image
+//    }()
+//
+//    private lazy var button: UIButton = {
+//        let button = UIButton()
+//        button.backgroundColor = .TablingPrimary
+//        button.setTitle("버튼", for: .normal)
+//        button.layer.cornerRadius = 24
+//        return button
+//    }()
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+        setAddTarget()
+        setRegisterCell()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+extension HistoryView {
+    func setUI() {
+        
+    }
+    
+    func setHierarchy() {
+    }
+    
+    func setLayout() {
+
+    }
+    
+    func setAddTarget() {
+
+    }
+    
+    @objc
+    func buttonTapped() {
+        
+    }
+    
+    func setRegisterCell() {
+        
+    }
+    
+    func setDataBind() {
+        
+    }
+}
+
--- SOPKATHON-iOS/SOPKATHON-iOS/Presentation/HistoryScene/ViewController/HistoryViewController.swift
+++ SOPKATHON-iOS/SOPKATHON-iOS/Presentation/HistoryScene/ViewController/HistoryViewController.swift
@@ -1,8 +1,76 @@
-//
-//  HistoryViewController.swift
-//  SOPKATHON-iOS
-//
-//  Created by 고아라 on 2023/11/26.
-//
+import UIKit
 
-import Foundation
+
+import SnapKit
+
+class HistoryViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    private let historyView = HistoryView()
+    private lazy var titleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "그땐 몇살이었지?"
+        label.textColor = .white
+        return label
+    }()
+    private lazy var mainBtn: UIBarButtonItem = {
+        let btn = UIBarButtonItem(barButtonSystemItem: .undo, target: self, action: #selector(goToMain))
+        btn.tintColor = .systemPink
+        return btn
+    }()
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        super.loadView()
+        
+        view = historyView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.titleView = titleView
+        self.navigationItem.leftBarButtonItem = mainBtn
+        self.navigationItem.leftItemsSupplementBackButton = true
+        setUI()
+        setHierarchy()
+        setLayout()
+        setDelegate()
+    }
+    
+    @objc
+    func goToMain(){
+        
+    }
+}
+
+// MARK: - Extensions
+extension HistoryViewController {
+    func setUI() {
+        
+    }
+    
+    func setHierarchy() {
+        titleView.addSubview(titleLabel)
+
+    }
+    
+    func setLayout() {
+        titleLabel.snp.makeConstraints{
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    func setDelegate() {
+        
+    }
+}
